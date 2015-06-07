dfecd6c8acb9ed28775a236609446c4350e69232
manuonda <manuonda@gmail.com>
2015-06-07 02:42:58 -0300

    modificate structure

---
+++ access/js/index.js
@@ -0,0 +1,11 @@
+jQuery(document).ready(function(){
+
+    console.log("load content");
+    var $=jQuery;
+    $("#sidebar").load("template/sidebar.html");
+    
+
+});
+
+
+
---
+++ access/news/js/content.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+
+   $("#content" ).load( "template/content.html" );
+    
+   $("header").load("template/header.html");    
+   
+   
+   loadTable();
+    
+    
+    
+});
