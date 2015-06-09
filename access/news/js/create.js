9edce9adfcbc73dc6005ceeccd7f6e8a8f28e4b8
manuonda <manuonda@gmail.com>
2015-06-09 19:56:42 -0300

    add laravel

---
+++ access/news/js/create.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+
+  
+   console.log("document ready create new");
+   $("#btnCrearNoticia").onclick(function(event)
+   {
+   	console.log("crear noticia");
+
+   });
+ 
+
+    
+    
+});
